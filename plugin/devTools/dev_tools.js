1088285726ecf984e3bd7325003546257212241b
PimiTree <voronenkotg@gmail.com>
2023-12-05 13:17:16 +0200

    CAS-6: slight reshufle

--- plugin/devTools/dev_tools.js
+++ plugin/devTools/dev_tools.js
@@ -1,16 +1,6 @@
-// chrome.devtools.panels.create("CAS", "128.png", "devTools/devPanel.html", function(CAS) {
-//     // Panel creation callback
-
-//     CAS.onShown.addListener(function(window) {
-//         console.log(chrome.windows);
-//         console.log(chrome.devtools);
-//     });
-// });
-
 chrome.devtools.panels.elements.createSidebarPane("CAS", function(CAS) {
     // Panel creation callback
-    CAS.setPage('devTools/dev_panel.html');
-    CAS.setHeight('8ex');  
+    CAS.setPage('devTools/dev_panel.html');  
 });
 
 
